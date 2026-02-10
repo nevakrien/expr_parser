feedfea38e2bd66ad52dbec082982d1ae4a647eb
Neva <nevo.krien@gmail.com>
2026-02-10 10:16:50 +0200

    diffrent hashes considered

--- src/identity_hasher.rs
+++ src/identity_hasher.rs
@@ -4,6 +4,14 @@ use std::hash::{BuildHasherDefault, Hasher};
 #[derive(Default)]
 pub struct IdentityHasher(u64);
 
+// fn hash(x: u64) -> u64 {
+//     let full = (x as u128).wrapping_mul(0x243f6a8885a308d3 as u128);
+//         let lo = full as u64;
+//         let hi = (full >> 64) as u64;
+//         lo ^ hi
+// }
+fn hash(x: u64) -> u64 {x}
+
 impl Hasher for IdentityHasher {
     fn write(&mut self, bytes: &[u8]) {
         let mut h = 0xcbf29ce484222325u64;
@@ -15,15 +23,15 @@ impl Hasher for IdentityHasher {
     }
 
     fn write_u8(&mut self, i: u8) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_u16(&mut self, i: u16) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_u32(&mut self, i: u32) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_u64(&mut self, i: u64) {
@@ -31,27 +39,27 @@ impl Hasher for IdentityHasher {
     }
 
     fn write_usize(&mut self, i: usize) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_i8(&mut self, i: i8) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_i16(&mut self, i: i16) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_i32(&mut self, i: i32) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_i64(&mut self, i: i64) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn write_isize(&mut self, i: isize) {
-        self.0 = i as u64;
+        self.0 = hash(i as u64);
     }
 
     fn finish(&self) -> u64 {
@@ -61,3 +69,4 @@ impl Hasher for IdentityHasher {
 
 type IdBuildHasher = BuildHasherDefault<IdentityHasher>;
 pub type IdHashMap<K, V> = HashMap<K, V, IdBuildHasher>;
+// pub type IdHashMap<K, V> = foldhash::HashMap<K,V>;
